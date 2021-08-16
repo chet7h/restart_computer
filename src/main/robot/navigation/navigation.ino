e613613597e72126c18b0360d616a12c841b1a5b
chet7h <chet7h@gmail.com>
2021-08-17 00:42:49 +0700

    cuonggnv20 day 4

--- src/main/robot/navigation/navigation.ino
+++ src/main/robot/navigation/navigation.ino
@@ -11,9 +11,13 @@
 // mã thiết bị. mỗi room sẽ chưa 1 robot. nó là ID tron table [room_info]
 #define DEVICE_ID  "1"
 #define VERSION  "1.0.0"
+#define TOKEN  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+
+//const String HOST = "http://baongochome.duckdns.org";
+//const int PORT = 11111;
 
 const String HOST = "http://192.168.1.5";
-const int PORT = 8080;
+const int PORT = 11111;
 
 //WIFI
 ESP8266WiFiMulti WiFiMulti;
@@ -46,13 +50,13 @@ void setup() {
   }
 
   WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP("Khu c", "123456790");
+  WiFiMulti.addAP("Baongoc", "baongoc@2018");
 
 }
 
 // khởi tạo là stop
 int status = 0;
-
+int count = 0;
 void loop() {
   // wait for WiFi connection
   if ((WiFiMulti.run() == WL_CONNECTED)) {
@@ -62,23 +66,31 @@ void loop() {
     HTTPClient http;
 
     Serial.print("[HTTP] begin...\n");
-    if (http.begin(client, HOST + ":" + PORT + "/robot?deviceId=" + DEVICE_ID)) {  // HTTP
+    String link_get =  HOST + ":" + PORT + "/hackathon/robot?deviceId=" + DEVICE_ID + "&token=" + TOKEN;
+    String link_update = HOST + ":" + PORT + "/hackathon/robot/updateWfAndBp?deviceId=" + DEVICE_ID + "&token=" + TOKEN + "&wifiSignal=" + WiFi.RSSI() + "&batteryPercentage=" + 100;
+    String link = "";
+    count ++;
+    if (count > 50) {
+      count = 0;
+      link = link_update;
+    } else {
+      link =  link_get;
+    }
+    if (http.begin(client, link)) { // HTTP
 
-
-      Serial.print("[HTTP] GET...\n");
-      Serial.print(HOST + ":" + PORT + "/robot?deviceId=" + DEVICE_ID);
+      Serial.println(link);
       // start connection and send HTTP header
       int httpCode = http.GET();
 
       // httpCode will be negative on error
       if (httpCode > 0) {
         // HTTP header has been send and Server response header has been handled
-        Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+        Serial.printf("[HTTP] GET... code: %d ", httpCode);
 
         // file found at server
         if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-          status = http.getString().toInt();
-          Serial.println(status);
+          status = http.getString().substring(1, 2).toInt();
+          Serial.printf("status: %d ", status);
         }
       } else {
         Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
@@ -94,31 +106,29 @@ void loop() {
     }
   }
 
-
-
   // naviation
   //0: stop | 1: trái | 2: phải | 3: lùi | 4: tiến | 999: update.
   switch (status) {
     case 0:    // stop
-      Serial.printf("stop");
+      Serial.println("stop");
       motors.stop();
       break;
     case 1:    // trái
-      Serial.printf("trái");
+      Serial.println("trái");
       motors.forwardA();
       motors.backwardB();
       break;
     case 2:    // phải
-      Serial.printf("phải");
+      Serial.println("phải");
       motors.forwardB();
       motors.backwardA();
       break;
     case 3:    // lùi
-      Serial.printf("lùi");
+      Serial.println("lùi");
       motors.backward();
       break;
     case 4:    // tiến
-      Serial.printf("tiến");
+      Serial.println("tiến");
       motors.forward();
       break;
   }
